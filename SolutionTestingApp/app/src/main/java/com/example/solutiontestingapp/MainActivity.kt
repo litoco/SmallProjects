1c16e40beb81df7ffa95682d2b9cee794d521d99
Ashutosh Kumar <ashutoshkr.1907@gmail.com>
2022-07-13 11:54:55 +0530

    bug fix

--- SolutionTestingApp/app/src/main/java/com/example/solutiontestingapp/MainActivity.kt
+++ SolutionTestingApp/app/src/main/java/com/example/solutiontestingapp/MainActivity.kt
@@ -7,11 +7,13 @@ import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
@@ -24,6 +26,7 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.solutiontestingapp.ui.theme.SolutionTestingAppTheme
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -48,17 +51,18 @@ fun AppUI() {
 
     Column (modifier = Modifier.fillMaxSize(1f)) {
         var text by remember { mutableStateOf("") }
-        val listScrollStateHolder = rememberLazyListState()
         val list = (0..99).toList()
         val scope = rememberCoroutineScope()
-        val keyboardVisible by rememberIsKeyboardOpen()
-        var firstVisibleItemIndexedValue by remember { mutableStateOf(0)}
-        var screenSize by remember { mutableStateOf(0)}
-        var scrollOffset by remember{ mutableStateOf(0)}
+        val listScrollStateHolder = rememberLazyListState(initialFirstVisibleItemIndex = list.size - 1)
+        val isKeyboardVisibleBefore = remember{ mutableStateOf(false)}
+        val lastVisibleItemIndex = remember{ mutableStateOf(0)}
+        val listItemAbsoluteSizeList = remember { (1..list.size).map { 0 }.toMutableList()}
+
+        rememberIsKeyboardOpen(scope, listScrollStateHolder, isKeyboardVisibleBefore, lastVisibleItemIndex, listItemAbsoluteSizeList)
 
         Box (modifier = Modifier
             .weight(1f)){
-            LazyColumn(state = listScrollStateHolder){
+            LazyColumn(state = listScrollStateHolder, modifier = Modifier){
                 items(list){ item ->
                     Text(modifier =  Modifier.fillMaxWidth(1f), text = "Item number: ${item + 1}", textAlign = TextAlign.Center)
                 }
@@ -73,47 +77,69 @@ fun AppUI() {
                 .fillMaxWidth(1f)
                 .focusable(true)
         )
-
-        LaunchedEffect(key1 = keyboardVisible){
-            if (listScrollStateHolder.layoutInfo.visibleItemsInfo.size - screenSize > 10){
-                screenSize = listScrollStateHolder.layoutInfo.visibleItemsInfo.size
-            }
-            if(keyboardVisible){
-                firstVisibleItemIndexedValue = listScrollStateHolder.firstVisibleItemIndex
-                scrollOffset = if(screenSize - listScrollStateHolder.layoutInfo.visibleItemsInfo.size > 0) screenSize + 1 - listScrollStateHolder.layoutInfo.visibleItemsInfo.size else 0
-                scope.launch {
-                    listScrollStateHolder.animateScrollToItem(firstVisibleItemIndexedValue + scrollOffset)
-                }
-            } else{
-                if (listScrollStateHolder.firstVisibleItemIndex + screenSize + 2 < list.size) {
-                    firstVisibleItemIndexedValue = if (listScrollStateHolder.firstVisibleItemIndex - scrollOffset > 0)  listScrollStateHolder.firstVisibleItemIndex - scrollOffset else 0
-                    scope.launch {
-                        listScrollStateHolder.animateScrollToItem(firstVisibleItemIndexedValue)
-                    }
-                }
-            }
-        }
     }
 }
 
 @Composable
-fun rememberIsKeyboardOpen(): State<Boolean> {
+fun rememberIsKeyboardOpen(
+    scope: CoroutineScope,
+    listScrollStateHolder: LazyListState,
+    isKeyboardVisibleBefore: MutableState<Boolean>,
+    lastVisibleItemIndex: MutableState<Int>,
+    listItemAbsoluteSizeList: MutableList<Int>
+): State<Boolean> {
     val view = LocalView.current
-    return produceState(initialValue = view.isKeyboardOpen()) {
+    return produceState(initialValue = view.isKeyboardOpen(scope, listScrollStateHolder, isKeyboardVisibleBefore, lastVisibleItemIndex, listItemAbsoluteSizeList)) {
         val viewTreeObserver = view.viewTreeObserver
-        val listener = ViewTreeObserver.OnGlobalLayoutListener { value = view.isKeyboardOpen() }
+        val listener = ViewTreeObserver.OnGlobalLayoutListener { value = view.isKeyboardOpen(
+            scope,
+            listScrollStateHolder,
+            isKeyboardVisibleBefore,
+            lastVisibleItemIndex,
+            listItemAbsoluteSizeList
+        )
+        }
         viewTreeObserver.addOnGlobalLayoutListener(listener)
         awaitDispose { viewTreeObserver.removeOnGlobalLayoutListener(listener)  }
     }
 }
 
 
-fun View.isKeyboardOpen(): Boolean {
+fun View.isKeyboardOpen(
+    scope: CoroutineScope,
+    listScrollStateHolder: LazyListState,
+    isKeyboardVisibleBefore: MutableState<Boolean>,
+    lastVisibleItemIndex: MutableState<Int>,
+    listItemAbsoluteSizeList: MutableList<Int>
+): Boolean {
     val rect = Rect()
     getWindowVisibleDisplayFrame(rect)
     val screenHeight = rootView.height
-    val keypadHeight = screenHeight - rect.bottom
-    return keypadHeight > screenHeight * 0.1
+    val absoluteKeyboardHeight = if (rect.bottom > 0) screenHeight - rect.bottom else 0
+    val visibleItemList = listScrollStateHolder.layoutInfo.visibleItemsInfo
+    if(visibleItemList.isNotEmpty()){
+        if(absoluteKeyboardHeight > 100){
+            if (!isKeyboardVisibleBefore.value){
+                scope.launch { listScrollStateHolder.scrollBy(absoluteKeyboardHeight.toFloat()) }
+                isKeyboardVisibleBefore.value = true
+            }
+            lastVisibleItemIndex.value = visibleItemList[visibleItemList.size-1].index
+            listItemAbsoluteSizeList[visibleItemList[visibleItemList.size-1].index] = visibleItemList[visibleItemList.size-1].size
+        } else {
+            if (isKeyboardVisibleBefore.value) {
+                var scrollOffset = 0
+                var i = listScrollStateHolder.layoutInfo.totalItemsCount - 1
+                while (i > lastVisibleItemIndex.value){
+                    scrollOffset += listItemAbsoluteSizeList[i]
+                    i -= 1
+                }
+                scope.launch {listScrollStateHolder.scrollBy(-scrollOffset.toFloat())}
+                isKeyboardVisibleBefore.value = false
+            }
+        }
+    }
+
+    return absoluteKeyboardHeight > screenHeight * 0.15
 }
 
 @Preview(showBackground = true)
