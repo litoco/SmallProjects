5077c593e231dd43a58a5a63ee37ab415e7b7bf0
Ashutosh Kumar <ashutoshkr.1907@gmail.com>
2022-09-22 16:54:33 +0530

    added text files for easy link accessing instead of modifying json files incorrectly

--- python_selenium_crawler/json_modifier.py
+++ python_selenium_crawler/json_modifier.py
@@ -1,11 +1,10 @@
 import json
 data = None
-with open("Questions.json") as filename:
+with open("Div2.json") as filename:
     data = filename.readlines()
 
-with open("Questions.json", "w") as filename:
+with open("Div2.txt", "w") as filename:
     for line in data:
         if "https" in line:
-            colon_place = line.index(":")
-            line = line[:colon_place] + line[colon_place:].replace('\"','')
+            line = line.replace('\"','')
         filename.write(line)
