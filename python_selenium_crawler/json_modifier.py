88a6ee44ded95fccc6c6d2429b04d95cb9d2b115
Ashutosh Kumar <ashutoshkr.1907@gmail.com>
2022-09-22 17:15:53 +0530

    Created md file to get better link access

--- python_selenium_crawler/json_modifier.py
+++ python_selenium_crawler/json_modifier.py
@@ -1,10 +1,38 @@
 import json
-data = None
-with open("Div2.json") as filename:
-    data = filename.readlines()
 
-with open("Div2.txt", "w") as filename:
-    for line in data:
-        if "https" in line:
-            line = line.replace('\"','')
-        filename.write(line)
+# markdown Div2's links
+# data = None
+# with open("Div2.json") as filename:
+#     data = json.load(filename)
+
+# with open("Div2.md", "w") as filename:
+#     filename.write("# Contests link")
+#     filename.write("\n")
+#     count = 1
+#     for url in data['urls']:
+#         if "https" in url:
+#             url = url.replace('\"','')
+#             url = str(count) + "." + url
+#             count += 1
+#         filename.write("\n")
+#         filename.write(url)
+
+
+# markdown questions link
+# data = None
+# with open("Questions.json") as filename:
+#     data = json.load(filename)
+
+# with open("Questions.md", "w") as filename:
+#     filename.write("# Codeforces questions")
+#     filename.write("\n")
+#     count = 1
+#     for info in data:
+#         code = info["problem_code"]
+#         name = info["problem_name"]
+#         url = info["problem_url"]
+#         url = url.replace('\"','')
+#         line = str(count) + "." + f"[{code}  {name}]({url})"
+#         count += 1
+#         filename.write("\n")
+#         filename.write(line)
