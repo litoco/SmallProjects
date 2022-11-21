f66cfe5f431d782ffdf6435221d07b5f46d488ff
Ashutosh Kumar <ashutoshkr.1907@gmail.com>
2022-11-21 22:42:07 +0530

    successfully migrated app to compose

--- MigrateItToCompose/app/src/main/java/com/example/migrateittocompose/MainActivity.kt
+++ MigrateItToCompose/app/src/main/java/com/example/migrateittocompose/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.migrateittocompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContent {
+            MaterialTheme {
+                MainScreen()
+            }
+        }
     }
+}
+
+@Composable
+fun MainScreen() {
+    Text(text = "Hello, there!!", modifier = Modifier.background(Color.Green))
 }
